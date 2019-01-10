e65f5cd22fd05479780de9c31bb3ce384d14591d
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-01-10 09:03:32 -0800

    minor updates to nav toggle

--- scripts/content.js
+++ scripts/content.js
@@ -11,13 +11,19 @@ function loadJSON(callback) {
   xobj.send(null);  
 }
 
+var open_modal = document.getElementById('nav-button');
+
 var introTxt = document.getElementById('intro-txt');
 var introH5 = document.getElementById('intro-h5');
 var introH4 = document.getElementById('intro-h4');
 
-var toggle = document.getElementsByClassName('toggleContent');
-for (var i = 0; i < toggle.length; i++) {
-    toggle[i].onclick = function() {
+// footer 
+var leftFooterContent = document.getElementById('panel-7-txt');
+var rightFooterContent = document.getElementById('modal-address');
+
+var toggleNav = document.getElementsByClassName('toggleNavigation');
+for (var i = 0; i < toggleNav.length; i++) {
+    toggleNav[i].onclick = function() {
         var id = this.id; 
         var data;
         loadJSON(function(json) {
@@ -27,6 +33,47 @@ for (var i = 0; i < toggle.length; i++) {
             // data[0] = home, [1] = about, [2] = staff, [3] = projects
             data = data[id];
 
+            /*
+            switch (id) {
+              case 'home':
+              // if id = home
+                // ...generate page elements 
+                  // ...import content
+
+                  // masthead = .backpanel-mh 
+                    // import = .frontpanel-mh 
+
+                  console.log('test');
+
+              break;
+              case 'about':
+              // if id = about 
+                // ...generate page elements 
+                  // ...import content
+
+                  // masthead = .backpanel-mh 
+                    // import = .frontpanel-mh
+
+                  console.log('test');
+
+              break;
+              case 'projects':
+              // if id = projects 
+                // ...generate page elements 
+                  // ...import content
+
+                  console.log('test');
+
+              break;
+              case 'contact':
+              // if id = contact 
+                // ...generate page elements 
+                  // ...import content 
+
+                  console.log('test');
+            }
+            */
+
             // intro masthead 
             introTxt.innerHTML = data.intro_masthead.header[0].content;
             introH5.innerHTML = data.intro_masthead.header[1].content;  
@@ -34,7 +81,15 @@ for (var i = 0; i < toggle.length; i++) {
 
             // panel 2
             // to do... 
-            
+
+            // footer 
+            /*
+            leftFooterContent = data.footer[0].content;
+            rightFooterContent = data.footer[1].content;
+            */
+
+            // close nav modal
+            open_modal.click();
         });
     };
 }
